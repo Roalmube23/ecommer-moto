0a21aff66e354f504676d04ddf05eaa4448c76cc
Roalmube23 <157928596+Roalmube23@users.noreply.github.com>
2024-03-02 15:59:46 -0500

    Update index.js
    cambio de codigo

--- PAGINA_WEB/index.js
+++ PAGINA_WEB/index.js
@@ -1,152 +1,107 @@
-document.addEventListener("DOMContentLoaded", function() {
-    /* const buttons = document.querySelectorAll('.comprar-btn');
-    buttons.forEach(button => {
-        button.addEventListener('click', addToCart);
-    });
-
-    function addToCart(event) {
-        const button = event.target;
-        const item = button.parentElement.parentElement;
-        const title = item.querySelector('p').textContent;
-        const price = item.querySelector('.precio').textContent;
-        const imgSrc = item.parentElement.querySelector('img').src;
-        
-        // Aquí puedes hacer lo que quieras con la información, por ejemplo, agregar el producto al carrito
-        console.log(`Añadido al carrito: ${title} ${price}`);
-    } */
-    pintar()
-    
-});
+//variables
+let allContainerCart = document.querySelector('.products');
+let containerBuyCart = document.querySelector('.card-items');
+let priceTotal = document.querySelector('.price-total')
+let amountProduct = document.querySelector('.count-product');
 
 
-let productosLenceria = [
-    {
-        nombre:"BMW S1000RR",
-        precio:"100.000.000",
-        imagen:"./BMW-S1000RR.jpeg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"BMW S1000RR",
-        precio:"100.000.000",
-        imagen:"./BMW-S1000RR.jpeg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"BMW S1000RR",
-        precio:"100.000.000",
-        imagen:"./BMW-S1000RR.jpeg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"NINJA ZX-10R",
-        precio:"100.000.000",
-        imagen:"./Kawasaki_Ninja_ZX-10.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"NINJA ZX-10R",
-        precio:"100.000.000",
-        imagen:"./Kawasaki_Ninja_ZX-10.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"NINJA ZX-10R",
-        precio:"100.000.000",
-        imagen:"./Kawasaki_Ninja_ZX-10.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"R1 M",
-        precio:"110.000.000",
-        imagen:"./R1M.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"R1 M",
-        precio:"110.000.000",
-        imagen:"./R1M.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"R1 M",
-        precio:"110.000.000",
-        imagen:"./R1M.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-    {
-        nombre:"R1 M",
-        precio:"110.000.000",
-        imagen:"./R1M.jpg",
-        descripcion:"MOTO DE 1000 CC",
-    },
-   
-]
+let buyThings = [];
+let totalCard = 0;
+let countProduct = 0;
 
+//functions
+loadEventListenrs();
+function loadEventListenrs(){
+    allContainerCart.addEventListener('click', addProduct);
 
-function pintar(){
-    productosLenceria.forEach((item,index)=>{
-        let card = document.createElement("div")
-        let imagen = document.createElement("img")
-        imagen.src= item.imagen
-        card.appendChild(imagen)
-        let div = document.createElement("div")
-        div.classList = "informacion"
-        card.appendChild(div)
-        let parrafo = document.createElement("p")
-        parrafo.textContent=item.nombre
-        div.appendChild(parrafo)
-        let precio = document.createElement("p")
-        precio.textContent=item.precio
-        div.appendChild(precio)
-        let descripcion = document.createElement("p")
-        descripcion.textContent=item.descripcion
-        div.appendChild(descripcion)
-        let talla = document.createElement("p")
-        talla.textContent=item.talla
-        div.appendChild(talla)
-       
-
-        const carrito = {
-            productos: [], // Array para almacenar los productos en el carrito
-        
-            // Método para añadir un producto al carrito
-            comprarProducto: function(e) {
-                e.preventDefault();
-                if (e.target.classList.contains('agregar-carrito')) {
-                    const productoSeleccionado = e.target.parentElement.parentElement;
-                    this.obtenerDatosProducto(productoSeleccionado);
-                }
-            },
-        
-            // Método para obtener los datos del producto seleccionado
-            obtenerDatosProducto: function(producto) {
-                const productoInfo = {
-                    imagen: producto.querySelector('img').src,
-                    nombre: producto.querySelector('p').textContent,
-                    precio: producto.querySelector('.precio').textContent,
-                    // Otros datos del producto que desees obtener
-                };
-                this.agregarProductoAlCarrito(productoInfo);
-            },
-        
-            // Método para agregar un producto al carrito
-            agregarProductoAlCarrito: function(producto) {
-                this.productos.push(producto);
-                console.log('Producto agregado al carrito:', producto);
-                // Aquí podrías actualizar la interfaz de usuario para reflejar el cambio en el carrito
-            }
-        };
-        
-
-
-
-
-        
-
-        
-
-        document.getElementById("contenedor").appendChild(card)
-
-    })
+    containerBuyCart.addEventListener('click', deleteProduct);
 }
+
+function addProduct(e){
+    e.preventDefault();
+    if (e.target.classList.contains('btn-add-cart')) {
+        const selectProduct = e.target.parentElement; 
+        readTheContent(selectProduct);
+    }
+}
+
+function deleteProduct(e) {
+    if (e.target.classList.contains('delete-product')) {
+        const deleteId = e.target.getAttribute('data-id');
+
+        buyThings.forEach(value => {
+            if (value.id == deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                totalCard =  totalCard - priceReduce;
+                totalCard = totalCard.toFixed(2);
+            }
+        });
+        buyThings = buyThings.filter(product => product.id !== deleteId);
+        
+        countProduct--;
+    }
+    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
+    if (buyThings.length === 0) {
+        priceTotal.innerHTML = 0;
+        amountProduct.innerHTML = 0;
+    }
+    loadHtml();
+}
+
+function readTheContent(product){
+    const infoProduct = {
+        image: product.querySelector('div img').src,
+        title: product.querySelector('.title').textContent,
+        price: product.querySelector('div p span').textContent,
+        id: product.querySelector('a').getAttribute('data-id'),
+        amount: 1
+    }
+
+    totalCard = parseFloat(totalCard) + parseFloat(infoProduct.price);
+    totalCard = totalCard.toFixed(2);
+
+    const exist = buyThings.some(product => product.id === infoProduct.id);
+    if (exist) {
+        const pro = buyThings.map(product => {
+            if (product.id === infoProduct.id) {
+                product.amount++;
+                return product;
+            } else {
+                return product
+            }
+        });
+        buyThings = [...pro];
+    } else {
+        buyThings = [...buyThings, infoProduct]
+        countProduct++;
+    }
+    loadHtml();
+    //console.log(infoProduct);
+}
+
+function loadHtml(){
+    clearHtml();
+    buyThings.forEach(product => {
+        const {image, title, price, amount, id} = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+            <img src="${image}" alt="">
+            <div class="item-content">
+                <h5>${title}</h5>
+                <h5 class="cart-price">${price}$</h5>
+                <h6>Amount: ${amount}</h6>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
+
+        containerBuyCart.appendChild(row);
+
+        priceTotal.innerHTML = totalCard;
+
+        amountProduct.innerHTML = countProduct;
+    });
+}
+ function clearHtml(){
+    containerBuyCart.innerHTML = '';
+ }
